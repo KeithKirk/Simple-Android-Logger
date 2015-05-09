a0aeabe2098b3269114806f7859e2cff3562ae5e
KeithKirk <kirk.keithm@gmail.com>
2015-05-09 22:14:11 +0100

    Added Debug Logger to LoggerFactory and modified how product ID's are calculated to allow easier subclassing.

--- library/src/main/java/com/kirk/simple_android_logger/LoggerFactory.java
+++ library/src/main/java/com/kirk/simple_android_logger/LoggerFactory.java
@@ -9,14 +9,16 @@ import android.content.Context;
  */
 public class LoggerFactory {
 
-    public static final int NULL_LOGGER = 0;
-    public static final int LOGCAT_LOGGER = 1;
+    public static final int NULL_LOGGER   = 0;
+    public static final int DEBUG_LOGGER  = NULL_LOGGER + 1;
+    public static final int LOGCAT_LOGGER = DEBUG_LOGGER + 1;
 
     /**
      * Returns a Logger implementation based on a given product id.
      * Current Products are:
      * <li> 0 = Null Logger
-     * <li> 1 = Logcat Logger
+     * <li> 1 = Debug Logger
+     * <li> 2 = Logcat Logger
      * Values outside this range will return a null Logger.
      *
      * @param id The id of the desired Logger implementation.
@@ -27,6 +29,8 @@ public class LoggerFactory {
         switch(id){
             case NULL_LOGGER:
                 return new NullLogger();
+            case DEBUG_LOGGER:
+                return new DebugLogger();
             case LOGCAT_LOGGER:
                 return new LogcatLogger();
             default:
