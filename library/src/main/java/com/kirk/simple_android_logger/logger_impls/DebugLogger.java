1f70458c5173b1f249f5c6fdaba16d0e44c21007
KeithKirk <kirk.keithm@gmail.com>
2015-05-20 20:57:37 +0100

    Removed unnecessary input.

--- library/src/main/java/com/kirk/simple_android_logger/logger_impls/DebugLogger.java
+++ library/src/main/java/com/kirk/simple_android_logger/logger_impls/DebugLogger.java
@@ -3,7 +3,6 @@ package com.kirk.simple_android_logger.logger_impls;
 import com.kirk.simple_android_logger.Log;
 import com.kirk.simple_android_logger.Logger;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
